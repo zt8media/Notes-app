1ab9b48b617e76f206006bf1a4480f6bf443c676
zt8media <zt8media@gmail.com>
2024-06-09 11:47:26 -0400

    comments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,66 +1,73 @@
+// Import the 'yargs' module for command-line argument parsing
 const yargs = require('yargs')
+// Import custom 'notes' module
 const notes = require('./notes.js')
 
-// Create add command
+// Create 'add' command
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
   builder: {
     title: {
       describe: 'Note title',
-      demandOption: true,
+      demandOption: true,  // Required option
       type: 'string'
     },
     body: {
       describe: 'Note body',
-      demandOption: true,
+      demandOption: true,  // Required option
       type: 'string'
     }
   },
   handler(argv) {
+    // Call 'addNote' function from 'notes' module
     notes.addNote(argv.title, argv.body)
   }
 })
 
-// Create remove command
+// Create 'remove' command
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
   builder: {
     title: {
       describe: 'Note title',
-      demandOption: true,
+      demandOption: true,  // Required option
       type: 'string'
     }
   },
   handler(argv) {
+    // Call 'removeNote' function from 'notes' module
     notes.removeNote(argv.title)
   }
 })
 
-// Create list command
+// Create 'list' command
 yargs.command({
   command: 'list',
   describe: 'List your notes',
   handler() {
+    // Call 'listNotes' function from 'notes' module
     notes.listNotes()
   }
 })
 
-// Create read command
+// Create 'read' command
 yargs.command({
   command: 'read',
   describe: 'Read a note',
   builder: {
     title: {
       describe: 'Note title',
-      demandOption: true,
+      demandOption: true,  // Required option
       type: 'string'
     }
   },
   handler(argv) {
+    // Call 'readNote' function from 'notes' module
     notes.readNote(argv.title)
   }
 })
 
+// Parse the command-line arguments
 yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,14 +1,23 @@
+// Import the 'fs' module to work with the file system
 const fs = require('fs')
+
+// Import the 'chalk' module to add colors to console output
 const chalk = require('chalk')
 
+// Function to get notes
 const getNotes = () => 'Your notes...'
 
+// Function to add a new note
 const addNote = (title, body) => {
+  // Load existing notes
   const notes = loadNotes()
+  // Check for duplicate titles
   const duplicateNotes = notes.filter(note => note.title === title)
 
+  // If no duplicate note found, add the new note
   if (duplicateNotes.length === 0) {
     notes.push({ title, body })
+    // Save the updated notes array to the JSON file
     saveNotes(notes)
     console.log(chalk.green.inverse('New note added!'))
   } else {
@@ -16,10 +25,14 @@ const addNote = (title, body) => {
   }
 }
 
-const removeNote = title => {
+// Function to remove a note
+const removeNote = (title) => {
+  // Load existing notes
   const notes = loadNotes()
+  // Filter out the note to be removed
   const notesToKeep = notes.filter(note => note.title !== title)
 
+  // If a note was removed, save the updated notes array
   if (notes.length > notesToKeep.length) {
     saveNotes(notesToKeep)
     console.log(chalk.green.inverse('Note removed!'))
@@ -28,16 +41,23 @@ const removeNote = title => {
   }
 }
 
+// Function to list all notes
 const listNotes = () => {
+  // Load existing notes
   const notes = loadNotes()
+  // Print each note title
   console.log(chalk.inverse('Your notes:'))
   notes.forEach(note => console.log(note.title))
 }
 
-const readNote = title => {
+// Function to read a specific note
+const readNote = (title) => {
+  // Load existing notes
   const notes = loadNotes()
+  // Find the note with the given title
   const note = notes.find(note => note.title === title)
 
+  // If note found, print the title and body
   if (note) {
     console.log(chalk.inverse(note.title))
     console.log(note.body)
@@ -46,21 +66,30 @@ const readNote = title => {
   }
 }
 
-const saveNotes = notes => {
+// Function to save notes to the JSON file
+const saveNotes = (notes) => {
+  // Convert notes array to JSON string
   const dataJSON = JSON.stringify(notes, null, 2)
+  // Write JSON string to 'notes.json' file
   fs.writeFileSync('notes.json', dataJSON)
 }
 
+// Function to load notes from the JSON file
 const loadNotes = () => {
   try {
+    // Read the JSON file
     const dataBuffer = fs.readFileSync('notes.json')
+    // Convert file contents to string
     const dataJSON = dataBuffer.toString()
+    // Parse JSON string to array
     return JSON.parse(dataJSON)
   } catch (e) {
+    // Return an empty array if file doesn't exist or can't be read
     return []
   }
 }
 
+// Export functions to be used in other files
 module.exports = {
   getNotes,
   addNote,
