b128e31408445ce816b70b3e2288878882c8d191
zt8media <zt8media@gmail.com>
2024-06-09 11:44:53 -0400

    Initial commit

---
+++ notes-app/notes.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => 'Your notes...'
+
+const addNote = (title, body) => {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter(note => note.title === title)
+
+  if (duplicateNotes.length === 0) {
+    notes.push({ title, body })
+    saveNotes(notes)
+    console.log(chalk.green.inverse('New note added!'))
+  } else {
+    console.log(chalk.red.inverse('Note title taken!'))
+  }
+}
+
+const removeNote = title => {
+  const notes = loadNotes()
+  const notesToKeep = notes.filter(note => note.title !== title)
+
+  if (notes.length > notesToKeep.length) {
+    saveNotes(notesToKeep)
+    console.log(chalk.green.inverse('Note removed!'))
+  } else {
+    console.log(chalk.red.inverse('No note found!'))
+  }
+}
+
+const listNotes = () => {
+  const notes = loadNotes()
+  console.log(chalk.inverse('Your notes:'))
+  notes.forEach(note => console.log(note.title))
+}
+
+const readNote = title => {
+  const notes = loadNotes()
+  const note = notes.find(note => note.title === title)
+
+  if (note) {
+    console.log(chalk.inverse(note.title))
+    console.log(note.body)
+  } else {
+    console.log(chalk.red.inverse('Note not found!'))
+  }
+}
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes, null, 2)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes,
+  addNote,
+  removeNote,
+  listNotes,
+  readNote
+}
---
+++ playground/1-json.js
@@ -0,0 +1,13 @@
+ const fs = require('fs')
+
+// const book = {
+//     title: "This is a book name",
+//     author: "This is the author of the book"
+// }
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync("1-json.json",bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJson = dataBuffer.toString()
+// const data = JSON.parse(dataJson)
+// console.log(data.title)
